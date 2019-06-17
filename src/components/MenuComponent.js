35b1f15a37a62a7dc2b7770bca8a4b68db7ce735
NastyaNasalevich <nastya.nasalevich@gmail.com>
2019-06-17 08:43:25 +0300

    W2 Single Page Applications Part 2

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import {Card, CardImg, CardImgOverlay, CardTitle} from 'reactstrap';
+import { Link } from 'react-router-dom';
 
     function RenderMenuItem({dish, onClick}) {
 
         return (
-            <Card onClick={() => onClick(dish.id)}>
+            <Card >
+                <Link to={`/menu/${dish.id}`} />
                 <CardImg width="100%" src={dish.image} alt={dish.name}/>
                 <CardImgOverlay>
                     <CardTitle>{dish.name}</CardTitle>
